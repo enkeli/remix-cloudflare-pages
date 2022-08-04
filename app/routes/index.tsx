b16ef2af03848c9f650a0a4156b54c35971b83c3
Gabriel Vargas Beltran <enkeli.dev>
2022-08-03 23:11:00 -0700

    Edited test preview deployment

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
 	return (
 		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-			<h1>Welcome to enkeli.dev cfpages</h1>
+			<h1>Welcome to enkeli.dev cfpages edited</h1>
 		</div>
 	);
 }
